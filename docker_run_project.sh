f7712717d4a04e15ec95a93c354d71f3e0a69dd4
latty <lionel.atty@gmail.com>
2017-10-04 08:03:26 +0200

    [docker] Le sample fonctionne avec la version glfw3. Modification du script docker_run_project.sh pour lancer la version du sample avec glfw3 (qui fonctionne sous docker, contrairement à la version de base avec freeglut+glew).

--- docker_run_project.sh
+++ docker_run_project.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
-sh _docker_run.sh "/home/project/run.sh"
+# sh _docker_run.sh "/home/project/run.sh"
+sh _docker_run.sh "/home/project/run_glfw3_sample.sh"
---
+++ run_glfw3_sample.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+./build/glfw3_sample
