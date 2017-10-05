29cf1b2aeba8c137ef6488278c2d07ca7313d185
ATTY Lionel <lionel.atty@holimetrix.com>
2017-10-06 00:31:24 +0200

    Fix&Update pour faire tourner le images/containers dockers sous ROG Ubuntu17.04 avec driver NVIDIA 375 (pas totalement à jour). Modification du from pour hériter d'une nvidia:cuda sous version 8.0 (au dela, il y a un pb de version de driver) mais en conservant une version ubuntu assez récente (> 16.04) sinon pb avec l'installation/compilation de glfw3 et glm. Du coup la target pour compiler/exécuter est Ubuntu > 16.04, Nvidia driver >= 375, CUDA <=8.0 (à tester plus finement).

--- docker/install_packages.sh
+++ docker/install_packages.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
 if [ -f "/etc/debian_version" ]; then
+    # pour ubuntu 14.04 (cuda 7.5)
+    # TODO: faudrait rajouter la clée publique du dépot !
+    # https://github.com/filipwasil/fillwave/issues/58
+    echo "deb http://ppa.launchpad.net/keithw/glfw3/ubuntu trusty main" | sudo tee -a /etc/apt/sources.list.d/fillwave_ext.list
+    echo "deb-src http://ppa.launchpad.net/keithw/glfw3/ubuntu trusty main" | sudo tee -a /etc/apt/sources.list.d/fillwave_ext.list
+    
+    # apt update
+
+    # force-yes car ppa/depot non authentifié.
+    
+    # apt-get install -y --force-yes \
+        # libglfw3-dev
+
     apt update && \
     apt-get install -y 	\
         build-essential		\
--- generate_traces.sh
+++ generate_traces.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+mkdir -p traces
+
 apitrace trace \
 	--output=traces/mytest3.trace \
 	./run.sh 
